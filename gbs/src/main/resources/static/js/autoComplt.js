8225ea1cb908681ad433450e097f440215d69840
Minhaz <rahmanoole.be@gmail.com>
2019-11-29 13:44:29 +0600

    up

--- gbs/src/main/resources/static/js/autoComplt.js
+++ gbs/src/main/resources/static/js/autoComplt.js
@@ -3,18 +3,10 @@ $(document).ready(function () {
         method: "GET",
         url: '/getAllSrchKeys',
         success: function (data) {
-            $("#second").append().html("");
-            for (var i = 0; i < data.length; i++) {
-                $("#second").append("<option value=\"" + data[i].value + "\">" + data[i] + "</option>");
-            }
-
-            autoCompletedTextField(data);
+            $("#search").autocomplete({
+                source: data
+            });
         }
     });
 });
 
-function autoCompletedTextField(tags) {
-    $("#search").autocomplete({
-        source: tags
-    });
-};
