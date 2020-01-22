c22d6184d082f9e40688295a98af449cce39ee1d
Minhaz <rahmanoole.be@gmail.com>
2020-01-22 23:59:57 +0600

    up

--- fcr/src/main/java/com/duny/fcr/controller/AdmissionPaymentController.java
+++ fcr/src/main/java/com/duny/fcr/controller/AdmissionPaymentController.java
@@ -44,7 +44,7 @@ public class AdmissionPaymentController {
         builder.setPrettyPrinting();
         Gson gson = new Gson();
         AdmissionPayment admissionPayment = gson.fromJson(admPaymentJson, AdmissionPayment.class);
-        //admissionPaymentRepo.save(admissionPayment);
+        admissionPaymentRepo.save(admissionPayment);
         return "redirect:/admissionFee";
     }
 
